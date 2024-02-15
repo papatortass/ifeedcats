91bce2b471ec2ab373a7acdbdfcf907641fa1fb6
Rafael Cunha Costa <a102526@uminho.pt>
2024-02-15 16:13:23 +0000

    Donation Alerts Support Added

---
+++ Donations/DAscript.py
@@ -0,0 +1,12 @@
+import socketio
+
+TOKEN = "You-DonationAlert-Token" #You donation alert token
+
+sio = socketio.Client()
+
+@sio.on('donation')
+def on_message(data):
+    print(data)
+
+sio.connect('wss://socket.donationalerts.ru:443')
+sio.emit('add-user', {"token": TOKEN, "type": "alert_widget"})
